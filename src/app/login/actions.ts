2f6f4cc860e5dd03c1082412e7ad410a8368c372
t-syunya <tsyunyam@gmail.com>
2026-04-18 16:03:45 +0900

    fix: await createClient for supabase ssr in server actions and components affecting build

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 export async function login(formData: FormData) {
-  const supabase = createClient();
+  const supabase = await createClient();
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
@@ -23,7 +23,7 @@ export async function login(formData: FormData) {
 }
 
 export async function signup(formData: FormData) {
-  const supabase = createClient();
+  const supabase = await createClient();
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
